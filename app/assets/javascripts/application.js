d21dfb421da5481cc229ea20002f5a624290b7a4
mtayllan <messiastayllan@gmail.com>
2020-03-08 08:48:17 -0300

    better layout'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,20 @@
 //= require bootstrap/dist/js/bootstrap.min
 //= require activestorage
 //= require turbolinks
-//= require sb_admin
+
+$(() => {
+  // Toggle the side navigation
+  $("#sidebarToggle").on("click", function(e) {
+    e.preventDefault();
+    $("body").toggleClass("sb-sidenav-toggled");
+  });
+});
+
+document.addEventListener('turbolinks:load', () => {
+  const path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+  $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+      if (this.href === path) {
+          $(this).addClass("active");
+      }
+  });
+});
