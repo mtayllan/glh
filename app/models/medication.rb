7c7f596389dc23ba80cfa076e086dec7dd991b7d
mtayllan <messiastayllan@gmail.com>
2020-03-08 11:36:17 -0300

    add graphic data

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -3,4 +3,6 @@ class Medication < ApplicationRecord
   belongs_to :medicine
 
   has_many :proceedings, dependent: :restrict_with_error
+
+  enum situation: { 'T1': 0, 'T2': 1, 'T3': 2 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-100.times do
-  Patient.create!(
+Patient.create!(
+  180.times.map {{
     name: Faker::Name.name,
     cpf: Faker::IDNumber.brazilian_citizen_number(formatted: true),
     rg: Faker::IDNumber.brazilian_id(formatted: true),
@@ -11,10 +11,31 @@
     number: Faker::Address.building_number,
     nacionality: Faker::Nation.nationality,
     civil_status: ['Casado(a)', 'Solteiro(a)', 'Viuvo(a)'].sample
-  )
+  }}
+)
+
+patients = Patient.all
+
+def sample_date
+  Time.zone.now - [1,2,3,4,5,6,7,8,9].sample.months
 end
 
 require_relative 'seed_data'
-Medicine.create!(medicines_seed * 100)
+Medicine.create!(medicines_seed * 30)
 
-User.create(name: 'user', email: 'u@u.com', password: '123456')
+Disease.create!(89.times.map { { name: Faker::Lorem.word, description: Faker::Lorem.sentence, cid: Faker::Number.number(digits: 10)  } })
+diseases = Disease.all
+medicines = Medicine.all
+Medication.create!(120.times.map { { disease_id: diseases.sample.id, medicine_id: medicines.sample.id, situation: Medication.situations.keys.sample } })
+
+medications = Medication.all
+Proceeding.create!(211.times.map { 
+  { 
+    medication_id: medications.sample.id,
+    patient_id: patients.sample.id,
+    number: Faker::Number.number(digits: 10),
+    created_at: sample_date }
+  }
+)
+
+User.create!(name: 'Admin', email: 'test@test.com', password: 'qwe123')
